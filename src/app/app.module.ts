e13952bc5fa5e14fd19f7c0aa8814e6e07cfd568
DarknessYamii <andresruizdelgado12@gmail.com>
2022-03-31 20:17:30 +0200

    header css html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ArticulosComponent } from './componentes/vistas/articulos/articulos.component';
@@ -17,12 +16,13 @@ import { FooterComponent } from './componentes/vistas/footer/footer.component';
     TiendaComponent,
     IndexComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
