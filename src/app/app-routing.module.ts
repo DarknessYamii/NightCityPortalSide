f0d74a308da32e761167cb0887dd02f0b8303579
DarknessYamii <andresruizdelgado12@gmail.com>
2022-03-31 18:50:39 +0200

    add routing index and shop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TiendaComponent } from './componentes/tienda/tienda.component';
+import { IndexComponent } from './componentes/index/index.component';
 
 
 const routes: Routes = [
+  { path: '', component: IndexComponent, pathMatch: 'full' },
+  { path: 'home', component: IndexComponent },
   { path: 'shop', component: TiendaComponent }
+  
 ];
 
 @NgModule({
