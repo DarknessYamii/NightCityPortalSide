e51e5d7981cb317a616c20466073b0f68a17c1d8
DarknessYamii <andresruizdelgado12@gmail.com>
2022-04-06 13:10:54 +0200

    Andres: navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TiendaComponent } from './componentes/tienda/tienda.component';
 import { IndexComponent } from './componentes/index/index.component';
+import { HeaderComponent } from './componentes/vistas/header/header.component';
 
 
 const routes: Routes = [
-  { path: '', component: IndexComponent, pathMatch: 'full' },
+  { path: '', component: HeaderComponent, pathMatch: 'full' },
   { path: 'home', component: IndexComponent },
   { path: 'shop', component: TiendaComponent }
   
