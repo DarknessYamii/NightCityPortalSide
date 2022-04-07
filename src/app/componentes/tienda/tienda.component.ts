2609fbc65b515d44859441fc8df7c67e46b0bad9
DarknessYamii <andresruizdelgado12@gmail.com>
2022-04-07 20:21:08 +0200

    Andres: tienda mod

--- src/app/componentes/tienda/tienda.component.ts
+++ src/app/componentes/tienda/tienda.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import articulos from "src/assets/api/articulos.json";
 @Component({
   selector: 'app-tienda',
   templateUrl: './tienda.component.html',
   styleUrls: ['./tienda.component.css']
 })
 export class TiendaComponent implements OnInit {
-
+  articulos: any = articulos;
   constructor() { }
 
   ngOnInit(): void {
