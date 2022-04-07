eee72962eaaa2f3e467a27bd23608898ac02f41d
DarknessYamii <andresruizdelgado12@gmail.com>
2022-04-07 20:24:37 +0200

    Andres: routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TiendaComponent } from './componentes/tienda/tienda.component';
 import { IndexComponent } from './componentes/index/index.component';
 import { HeaderComponent } from './componentes/vistas/header/header.component';
-import { ArticulosComponent } from './componentes/vistas/articulos/articulos.component';
-
 
 const routes: Routes = [
   { path: '', component: HeaderComponent, pathMatch: 'full' },
   { path: 'home', component: IndexComponent },
-  { path: 'shop', component: ArticulosComponent }
+  { path: 'shop', component: TiendaComponent }
   
 ];
 
